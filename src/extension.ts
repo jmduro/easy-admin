74a5892455f1ace0af1a495d09071c852b62b3f9
Alejojooo <alejandrofn2204@gmail.com>
2023-11-10 17:20:18 -0600

    Vista con atributos

--- src/extension.ts
+++ src/extension.ts
@@ -1,42 +1,38 @@
 import * as vscode from 'vscode';
-import { TareaTreeProviderView } from './treeview/tareaProvider';
-import { TareaService } from "./treeview/tareaService";
-import { Tarea } from './treeview/tarea';
+import { TareaProvider } from './treeview/tarea';
 
 export function activate(context: vscode.ExtensionContext) {
-	const tareaService = new TareaService();
-	const treeDataProvider = new TareaTreeProviderView(tareaService);
+	const tareaProvider = new TareaProvider();
+	vscode.window.createTreeView('tareas', { treeDataProvider: tareaProvider });
 
-	vscode.window.createTreeView('tareas', { treeDataProvider });
+	// vscode.commands.registerCommand('easy-admin.agregarTarea', () => {
+	// 	vscode.window.showInputBox({ prompt: 'Agregar una nueva tarea' }).then(nombre => {
+	// 		if (nombre) {
+	// 			tareaService.agregarTarea(nombre);
+	// 			tareaProvider.refresh();
+	// 		}
+	// 	});
+	// });
 
-	vscode.commands.registerCommand('easy-admin.agregarTarea', () => {
-		vscode.window.showInputBox({ prompt: 'Agregar una nueva tarea' }).then(nombre => {
-			if (nombre) {
-				tareaService.agregarTarea(nombre);
-				treeDataProvider.refresh();
-			}
-		});
-	});
+	// vscode.commands.registerCommand('easy-admin.eliminarTarea', (nodo: vscode.TreeItem) => {
+	// 	const label = nodo.label as string;
+	// 	const index = tareaService.getTareas().findIndex(tarea => tarea.nombre === label);
 
-	vscode.commands.registerCommand('easy-admin.eliminarTarea', (nodo: vscode.TreeItem) => {
-		const label = nodo.label as string;
-		const index = tareaService.getTareas().findIndex(tarea => tarea.nombre === label);
+	// 	if (index !== -1) {
+	// 		tareaService.eliminarTarea(index);
+	// 		tareaProvider.refresh();
+	// 	}
+	// });
 
-		if (index !== -1) {
-			tareaService.eliminarTarea(index);
-			treeDataProvider.refresh();
-		}
-	});
+	// vscode.commands.registerCommand('easy-admin.alternarEstado', (nodo: vscode.TreeItem) => {
+	// 	const label = nodo.label as string;
+	// 	const index = tareaService.getTareas().findIndex(tarea => tarea.nombre === label);
 
-	vscode.commands.registerCommand('easy-admin.alternarEstado', (nodo: vscode.TreeItem) => {
-		const label = nodo.label as string;
-		const index = tareaService.getTareas().findIndex(tarea => tarea.nombre === label);
-
-		if (index !== -1) {
-			tareaService.alternarEstado(index);
-			treeDataProvider.refresh();
-		}
-	});
+	// 	if (index !== -1) {
+	// 		tareaService.alternarEstado(index);
+	// 		tareaProvider.refresh();
+	// 	}
+	// });
 }
 
 // This method is called when your extension is deactivated
